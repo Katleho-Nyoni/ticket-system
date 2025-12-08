941ad96ad7b389ba3e35865fda9a980edee26f3a
Katleho-Nyoni <eaglenyoni@gmail.com>
2025-12-08 17:52:12 +0200

    Mockup

--- root/frontend/src/pages/Login.jsx
+++ root/frontend/src/pages/Login.jsx
@@ -1,11 +1,28 @@
-// import firebase from "firebase";
+import { useState } from "react";
+import { Form, Link } from "react-router-dom";
 
+export default function TicketForm(){
+    // [employeeName, setName] = useState("");
+    // [department, setDepartment] = useState("");
+    // [issueType, setIssueType] = useState("");
+    // [description, setDescription] = useState("");
 
+    function LoginPage(){
 
-export default function LoginPage() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Login Page</h1>
-    </>
-  );
+    }
+
+    return(
+        <>
+            <div className="flex flex-col justify-center items-center m-auto mt-30 gap-4 h-70 w-170">
+                <h1 className="text-3xl font-bold">Sign In</h1>
+                <Form className="flex flex-col justify-center gap-4 rounded ">
+                  <input type="email" name="email" id="email" placeholder="example.email.com" required className="py-2 rounded border border-amber-500"/>
+                  <input type="password" name="password" id="password" placeholder="password" required className="py-2 rounded border border-amber-500"/>
+                  <button type="submit" className="bg-blue-500 text-white font-bold py-2 px-8  self-center rounded-3xl cursor-pointer hover:bg-blue-600">Log In</button>
+
+                  <Link to="auth.google.com" className="py-2 px-6 bg-gray-400 rounded-xl">Continue with Google</Link>
+                </Form>
+            </div>
+        </>
+    );
 }
--- root/frontend/src/pages/TicketForm.jsx
+++ root/frontend/src/pages/TicketForm.jsx
@@ -1,33 +1,44 @@
-
+import { useState } from "react";
+import { Form } from "react-router-dom";
 
 export default function TicketForm(){
+    // [employeeName, setName] = useState("");
+    // [department, setDepartment] = useState("");
+    // [issueType, setIssueType] = useState("");
+    // [description, setDescription] = useState("");
+
+    function SubmitForm(){
+
+    }
+
     return(
         <>
-            <div className="flex flex-col justify-center m-26 gap-4 boxshadow-lg p-8 rounded ">
+            <div className="flex flex-col justify-center items-center m-auto mt-30 gap-4 h-70 w-170">
                 <h1 className="text-3xl font-bold">Log a Ticket</h1>
-            <div>
-                <input type="text" id="name" name="name" placeholder="Employee Name" required className="mt-6 border border-blue-600 py-2 px-18 rounded"/>
-            </div>
-            <summary className="list-none">
-                <select name="software-hardware" id="software-hardware">
-                    <option value="">default</option>
-                    <option value="hardware">Hardware</option>
-                    <option value="software">Software</option>
-                </select>
-            </summary>
-           
-            <summary className="list-none">
-                <select name="department" id="department">
-                    <option value="">Department</option>
-                    <option value="marketing">Marketing</option>
-                    <option value="human-resource">Human Resource(HR)</option>
-                    <option value="tech">Tech</option>
-                </select>
-            </summary>
-            <div>
-                <textarea id="description" name="description" rows={6} placeholder="Leave brief description here..." required className="border border-blue-600 py-2 px-18 rounded"></textarea>
-            </div>
-            <button type="submit" className="bg-blue-500 text-white font-bold py-2 px-4 w-auto self-center rounded cursor-pointer hover:bg-blue-600">Submit Ticket</button>
+                <Form className="flex flex-col justify-center gap-4 rounded ">
+                    <input type="text" id="name" name="name" placeholder="Employee Name" required className="mt-6 border border-blue-600 py-2 px-18 rounded"/>
+            
+                    <summary className="list-none ">
+                    <select name="software-hardware" id="software-hardware">
+                        <option value="">default</option>
+                        <option value="hardware">Hardware</option>
+                        <option value="software">Software</option>
+                    </select>
+                    </summary>
+            
+                    <summary className="list-none">
+                    <select name="department" id="department">
+                        <option value="">Department</option>
+                        <option value="marketing">Marketing</option>
+                        <option value="human-resource">Human Resource</option>
+                        <option value="tech">Tech</option>
+                    </select>
+                    </summary>
+                
+                    <textarea id="description" name="description" rows={6} placeholder="Leave brief description here..." required className="border border-blue-600 py-2 px-18 rounded"></textarea>
+                
+                    <button type="submit" onSubmit={SubmitForm} className="bg-blue-500 text-white font-bold py-2 px-4 w-auto self-center rounded cursor-pointer hover:bg-blue-600">Submit Ticket</button>
+                </Form>
             </div>
         </>
     );

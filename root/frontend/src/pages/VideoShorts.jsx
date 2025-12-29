a2beca37a7dc9723e8ccc7f4d3e87f9bb8630e7d
Katleho-Nyoni <eaglenyoni@gmail.com>
2025-12-29 18:53:59 +0200

    Adding background image and styling

--- root/frontend/src/pages/VideoShorts.jsx
+++ root/frontend/src/pages/VideoShorts.jsx
@@ -20,19 +20,21 @@ export default function VideoShortsApp(){
 
   return (
     <>
-      <div className=' flex flex-col gap-4 '>
-        <h1 className='font-extrabold text-5xl m-10'>Video Shorts</h1>
-        {error && <p className='text-red-600'>{error}</p>}
-        <div className='grid grid-cols-3 gap-10'>
-          {data.map((video) => (
-            <div key={video.id} >
-              <video src={video.video_url} height={350} 
-              width={350} poster={video.thumbnail} controls
-              className='rounded-t-xl' />
-              <h2 className='font-bold mt-2'>{video.name} </h2>
+      <div className='w-screen h-screen bg-[url(/src/assets/video.webp)] bg-center bg-cover bg-no-repeat'>
+            <div className=' flex flex-col gap-4'>
+                <h1 className='font-extrabold text-5xl m-10'>Video Shorts</h1>
+                {/* {error && <p className='text-red-600'>{error}</p>} */}
+                {/* <div className='grid grid-cols-3 gap-10'>
+                    {data.map((video) => (
+                        <div key={video.id} >
+                        <video src={video.video_url} height={350} 
+                        width={350} poster={video.thumbnail} controls
+                        className='rounded-t-xl' />
+                        <h2 className='font-bold mt-2'>{video.name} </h2>
+                        </div>
+                    ))}
+                </div> */}
             </div>
-          ))}
-        </div>
       </div>
     </>
   )

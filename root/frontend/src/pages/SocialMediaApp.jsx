fd59502aa61264d78e7e991d4f198d334fc9e6f6
Katleho-Nyoni <eaglenyoni@gmail.com>
2025-12-29 18:02:43 +0200

    Styling the sign out button

--- root/frontend/src/pages/SocialMediaApp.jsx
+++ root/frontend/src/pages/SocialMediaApp.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function SocialMediaApp(){
 
     return(
-        <>
-            <h1>Social Media App</h1>
+        <>  
+            <button className="sign-out-btn">Sign Out</button>
+            <div className="h-screen w-auto flex flex-col justify-center items-center gap-6">
+                <h1 className="font-bold text-4xl">APP UNDER CONSTRUCTION</h1>
+                <p>Explore other apps in the meantime</p>
+                <Link to='/weather-app'><button className="btn-redirect">Weather App</button></Link>
+                <Link to='/ticket-system'><button className="btn-redirect">Ticket System</button></Link>
+                <Link to='/video-shorts'><button className="btn-redirect">Video Shorts</button></Link>
+            </div>
         </>
     );
 }

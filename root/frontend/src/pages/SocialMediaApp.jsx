e10e921b5fb75f27248b56112d73f6951f12a3ba
Katleho-Nyoni <eaglenyoni@gmail.com>
2025-12-29 18:07:41 +0200

    adding background image & making singout button more rounded

--- root/frontend/src/pages/SocialMediaApp.jsx
+++ root/frontend/src/pages/SocialMediaApp.jsx
@@ -4,13 +4,15 @@ export default function SocialMediaApp(){
 
     return(
         <>  
-            <button className="sign-out-btn">Sign Out</button>
-            <div className="h-screen w-auto flex flex-col justify-center items-center gap-6">
-                <h1 className="font-bold text-4xl">APP UNDER CONSTRUCTION</h1>
-                <p>Explore other apps in the meantime</p>
-                <Link to='/weather-app'><button className="btn-redirect">Weather App</button></Link>
-                <Link to='/ticket-system'><button className="btn-redirect">Ticket System</button></Link>
-                <Link to='/video-shorts'><button className="btn-redirect">Video Shorts</button></Link>
+            <div className="bg-[url(/src/assets/social.webp)] bg-center bg-cover">
+                <button className="sign-out-btn">Sign Out</button>
+                <div className="h-screen w-auto flex flex-col justify-center items-center gap-6">
+                    <h1 className="font-bold text-4xl">APP UNDER CONSTRUCTION</h1>
+                    <p>Explore other apps in the meantime</p>
+                    <Link to='/weather-app'><button className="btn-redirect">Weather App</button></Link>
+                    <Link to='/ticket-system'><button className="btn-redirect">Ticket System</button></Link>
+                    <Link to='/video-shorts'><button className="btn-redirect">Video Shorts</button></Link>
+                </div>
             </div>
         </>
     );

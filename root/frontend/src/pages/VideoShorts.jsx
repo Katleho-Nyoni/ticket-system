0f60939b6584eebec57cb2ddeaaa2243860ee586
Katleho-Nyoni <eaglenyoni@gmail.com>
2025-12-29 18:42:31 +0200

    Copy paste logic from the  video shorts app project

--- root/frontend/src/pages/VideoShorts.jsx
+++ root/frontend/src/pages/VideoShorts.jsx
@@ -1,8 +1,39 @@
-export default function VideoShortsApp(){
+import { useEffect, useState } from 'react';
 
-    return(
-        <>
-            <h1>Video Shorts App</h1>
-        </>
-    );
+export default function VideoShortsApp(){
+ const [data, setData] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/API/video');
+        const results = await response.json();
+        setData(results);
+        console.log(results);
+      } catch (err) {
+        setError(err.message);
+      }
+    };
+    fetchData();
+  },[]);
+
+  return (
+    <>
+      <div className=' flex flex-col gap-4 '>
+        <h1 className='font-extrabold text-5xl m-10'>Video Shorts</h1>
+        {error && <p className='text-red-600'>{error}</p>}
+        <div className='grid grid-cols-3 gap-10'>
+          {data.map((video) => (
+            <div key={video.id} >
+              <video src={video.video_url} height={350} 
+              width={350} poster={video.thumbnail} controls
+              className='rounded-t-xl' />
+              <h2 className='font-bold mt-2'>{video.name} </h2>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  )
 }

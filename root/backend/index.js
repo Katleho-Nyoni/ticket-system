99f51dc0df234369f9459b27632b2660fd0a9647
Katleho-Nyoni <eaglenyoni@gmail.com>
2025-12-29 18:58:42 +0200

    copy paste logic of the video shorts app's server

--- root/backend/index.js
+++ root/backend/index.js
@@ -1,12 +1,22 @@
-import express from 'express';  
+import express from 'express';
+// import rate from 'express-rate-limit';
 
 const app = express();
 
 app.use(express.json());
 
+const API_URL = 'https://orangevalleycaa.org/api/videos';
 
-
-
-app.listen(5050, () => {
-    console.log('Server is running on port 5050');
+app.get('/API/video', async (req,res) => {
+    try {
+         const response =await fetch(API_URL);
+         const data = await response.json();
+         res.json(data);
+    } catch (error) {
+        console.error(error.message);
+    }
 })
+
+app.listen(3000, () => {
+    console.log('Server is running on port 30000');
+});

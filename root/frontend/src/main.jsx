3ea58964c0f6c62060fb85058583ecca6f666bdd
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-01-01 18:52:26 +0200

    imorting info

--- root/frontend/src/main.jsx
+++ root/frontend/src/main.jsx
@@ -1,11 +1,11 @@
-import "firebase/analytics";
-import "firebase/auth";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
+import "firebase/app"
+import "firebase/auth"
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyBOmFKYokDt_1ETFj_X2KoNDJNERZffgJI",
@@ -19,6 +19,10 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, googleProvider };
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

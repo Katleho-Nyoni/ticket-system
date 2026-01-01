c7c4d343f60297dbbc7129284df64e0b403fda71
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-01-01 15:03:58 +0200

    Serving videos from backend

--- root/backend/index.js
+++ root/backend/index.js
@@ -1,24 +1,40 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
 // import rate from 'express-rate-limit';
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors({
     origin: 'http://localhost:5173',
-    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    // credentials: true,
 }));
 
 app.use(express.json());
 
+const WEATHER_CONDITION_API_KEY = process.env.WEATHER_API_KEY;
+
+const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${Location}&appid=${WEATHER_CONDITION_API_KEY}&units=metric`;
+
+app.get('/API/weather', async (req, res) => {
+    try {
+        const results = await fetch(WEATHER_API_URL);
+        const data = await results.json();
+        res.json(data);
+    } catch (error) {
+        console.error(error.message);
+    }
+})
 
 
-
-const API_URL = 'https://orangevalleycaa.org/api/videos';
+const VIDEO_API_URL = 'https://orangevalleycaa.org/api/videos';
 
 app.get('/API/video', async (req,res) => {
     try {
-         const response =await fetch(API_URL);
+         const response =await fetch(VIDEO_API_URL);
          const data = await response.json();
          res.json(data);
     } catch (error) {

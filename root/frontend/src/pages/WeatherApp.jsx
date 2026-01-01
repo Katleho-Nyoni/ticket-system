6dd61f60d16d988ef818baf35c54d83893a12341
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-01-01 17:43:52 +0200

    Serving a button dynamically

--- root/frontend/src/pages/WeatherApp.jsx
+++ root/frontend/src/pages/WeatherApp.jsx
@@ -7,6 +7,7 @@ export default function WeatherApp() {
   const [location,setLocation] = useState("")
   const [error, setError] = useState("")
   const [data, setData] = useState({})
+  const [buttonExpand, setButtonExpand] = useState(false)
 
   // console.log(url)
   const searchLocation = async (query) => {
@@ -36,10 +37,18 @@ export default function WeatherApp() {
     <>
       <div className='h-screen w-auto bg-[url(/src/assets/landscape.webp)] bg-center bg-cover'>
         <div id="search" className='flex flex-row justify-center p-4 '>
-          <input type="search" value={location} onChange={e => setLocation(e.target.value)} placeholder='Enter Location'
-          className='bg-white text-gray-500 py-2 ' />
-          <button type="submit" className='p-2 bg-blue-700 font-bold hover:bg-blue-800' 
-          onClick={() => searchLocation(location)}>Search</button>
+          {!buttonExpand ? (
+            <button className='rounded-full border border-green-600 bg-green-600 py-2 px-4 font-bold text-white cursor-pointer'
+            onClick={() => {setButtonExpand(true)}}>Search Location</button>
+          ) : (
+              <>
+                <input type="search" value={location} onChange={e => setLocation(e.target.value)} placeholder='Enter Location'
+                className='bg-white text-gray-500 py-2 ' />
+                <button type="submit" className='p-2 bg-blue-700 font-bold hover:bg-blue-800' 
+                onClick={() => searchLocation(location)}>Search</button>
+              </>
+          )}
+          
         </div>
         {error && <p className='text-red-600'>{error}</p> }
         <div id="container" className='m-auto h-96 top-1/10 p-12 text-black relative flex flex-col justify-between'>

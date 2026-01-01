8194c99a1490b9775941e6f35760a582625d85ab
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-01-01 19:01:32 +0200

    Debug path

--- root/frontend/src/App.jsx
+++ root/frontend/src/App.jsx
@@ -3,7 +3,7 @@ import './styles/App.css';
 import Navigation from './pages/Nav';
 import HomePage from './pages/Home';
 import AdminDashboardPage from './pages/AdminDashboard';
-import LoginPage from './pages/Login';
+import SignInPage from './pages/signIn';
 import TicketForm from './pages/TicketForm';
 import WeatherApp from './pages/WeatherApp';
 import SocialMediaApp from './pages/SocialMediaApp';
@@ -15,7 +15,7 @@ const routes = [{
   path: '/', element: <Navigation /> },{
   path: '/', index: true, element: <HomePage /> },{
   path: '/admin/login', element: <AdminDashboardPage /> },{
-  path: 'user/login', element: <LoginPage /> },{
+  path: 'user/login', element: <SignInPage /> },{
   path: '/log/ticket', element: <TicketForm /> },{
   path: '/weather-app', element: <WeatherApp /> },{
   path: '/social-media', element: <SocialMediaApp /> },{

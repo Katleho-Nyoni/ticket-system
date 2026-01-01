c4c762a4f864f94e0b3a768e6c8364be2561993b
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-01-01 15:04:15 +0200

    Creating the loader

---
+++ root/frontend/src/loaders/VideoShortsLoader.jsx
@@ -0,0 +1,28 @@
+export default function VideoShortsLoader(){
+
+    return(
+        <>
+            <div className="grid grid-cols-3 gap-20 m-10 p-8">
+                <div className=" video-loader">
+                    <div className="loading-skeleton"></div>
+                </div>
+                <div className=" video-loader">
+                    <div className="loading-skeleton"></div>
+                </div>
+                <div className=" video-loader">
+                    <div className="loading-skeleton"></div>
+                </div>
+                <div className=" video-loader">
+                    <div className="loading-skeleton"></div>
+                </div>
+                <div className=" video-loader">
+                    <div className="loading-skeleton"></div>
+                </div>
+                <div className=" video-loader">
+                    <div className="loading-skeleton"></div>
+                </div>
+
+            </div>
+        </>
+    );
+}

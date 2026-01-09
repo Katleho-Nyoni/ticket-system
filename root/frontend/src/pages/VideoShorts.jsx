fcda3ad14077ef12524a95fc5b7acce1fd49d7bc
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-01-10 00:42:05 +0200

    Creating loader + video shorts component😭🙂

--- root/frontend/src/pages/VideoShorts.jsx
+++ root/frontend/src/pages/VideoShorts.jsx
@@ -1,11 +1,56 @@
 import { useEffect, useState } from 'react';
-import { useNavigation } from 'react-router-dom';
+// import { useNavigation } from 'react-router-dom';
 import { TransparentNav } from './Nav';
 
+function VideoLoader(){
+  return(
+    <>
+      <div className="grid grid-cols-3 gap-20 m-10 p-8">
+        <div className=" video-loader">
+          <div className="loading-skeleton"></div>
+        </div>
+        <div className=" video-loader">
+          <div className="loading-skeleton"></div>
+        </div>
+        <div className=" video-loader">
+          <div className="loading-skeleton"></div>
+        </div>
+        <div className=" video-loader">
+          <div className="loading-skeleton"></div>
+        </div>
+        <div className=" video-loader">
+          <div className="loading-skeleton"></div>
+        </div>
+        <div className=" video-loader">
+          <div className="loading-skeleton"></div>
+        </div>
 
+      </div>
+    </>
+  );
+}
+
+function VideoShorts({videos}){
+
+  return(
+    <>
+      <div className='grid grid-cols-3 gap-10'>
+        {videos.map((video) => (
+          <div key={video.id} >
+            <video src={video.video_url} height={350} 
+            width={350} poster={video.thumbnail} controls
+            className='rounded-t-xl' />
+            <h2 className='font-bold mt-2'>{video.name} </h2>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
 
 export default function VideoShortsApp(){
- const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true)
+  const [data, setData] = useState([]);
   const [error, setError] = useState('');
 
   useEffect(() => {
@@ -14,16 +59,14 @@ export default function VideoShortsApp(){
         const response = await fetch('http://localhost:3000/API/video');
         const results = await response.json();
         setData(results);
-        console.log(results);
+        setIsLoading(false);
       } catch (err) {
         setError(err.message);
+        setIsLoading(false);
       }
     };
     fetchData();
   },[]);
-
-
-  const VideoShortsData = () => {
 
     return (
    <>
@@ -32,29 +75,9 @@ export default function VideoShortsApp(){
             <div className=' flex flex-col gap-4 mb-6'>
                 <h1 className='font-extrabold text-5xl m-10'>Video Shorts</h1>
                 {error && <p className='text-red-600 text-xl'>{error}</p>}
-                <div className='grid grid-cols-3 gap-10'>
-                    {data.map((video) => (
-                        <div key={video.id} >
-                        <video src={video.video_url} height={350} 
-                        width={350} poster={video.thumbnail} controls
-                        className='rounded-t-xl' />
-                        <h2 className='font-bold mt-2'>{video.name} </h2>
-                        </div>
-                    ))}
-                </div>
+              {isLoading ? <VideoLoader /> : <VideoShorts videos={data} />}  
             </div>
       </div>
     </>
     );
-  }
-
-  const navigation = useNavigation()
-
-  const isLoading = navigation.state === "loading"
-
-  return (
-    <>
-      {isLoading ? <VideoShortsLoader /> : <VideoShortsData />}
-    </>
-  )
 }

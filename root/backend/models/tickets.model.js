934ebba7f7c336003b15d8abba5390a14f0dd2db
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-01-12 18:53:37 +0200

    Creating the ticket schema

--- root/backend/models/tickets.model.js
+++ root/backend/models/tickets.model.js
@@ -1,6 +1,32 @@
 import mongoose from 'mongoose'
 
-const TicketSchema = mongoose.Schema({},{
+const TicketSchema = mongoose.Schema({
+    employeeName : {
+        type: String,
+        required: true,
+        default: "Undocumented Employee"
+    },
+    employeeEmail : {
+        type : String,
+        required : true,
+        default : "employee@company.com"
+    },
+    issueType : {
+        type : String,
+        required : true,
+        default : "Software"
+    },
+    description : {
+        type : String,
+        required : true,
+        default : "Briefly describe your issue"
+    },
+    status : {
+        type : String,
+        required : true,
+        default : "Open"
+    }
+},{
     timestamps: true
 });
 

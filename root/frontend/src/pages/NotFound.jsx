f06d5b942610f598614ed088bea981783e1b1529
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-01-28 21:11:24 +0200

    Creation & styling of page

---
+++ root/frontend/src/pages/NotFound.jsx
@@ -0,0 +1,13 @@
+export default function NotFoundPage(){
+    return(
+        <>
+            <div className="h-screen w-screen flex flex-col justify-center items-center">
+                <div className="flex flex-row">
+                    <h1 className="font-extrabold text-5xl mr-5">404</h1>
+                    <h2 className="font-bold text-3xl">Page Not Found</h2>
+                </div>
+                <p>The page you are looking for does not exist.</p>
+            </div>
+        </>
+    );
+}

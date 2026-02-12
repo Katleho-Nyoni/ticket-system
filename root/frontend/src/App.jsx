edc69e710a235a4b9127fdf08f9bd97a50c3f1cb
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-02-13 00:26:09 +0200

    Protecting the routes

--- root/frontend/src/App.jsx
+++ root/frontend/src/App.jsx
@@ -14,21 +14,24 @@ import UserProfilePage from './pages/userProfile';
 import VideoShortsById from './pages/VideoShortsById';
 import NotFoundPage from './pages/NotFound';
 import { fetchTickets } from './pages/AdminDashboard';
+import { PrivateRoute } from './auth/PrivateRoute';
 
 const routes = [{
-  path: '/', element: <HomePage />, errorElement: <NotFoundPage /> },{
   path: 'user/login', element: <SignInPage /> },{
+  element: (<PrivateRoute />),
+  children:[{
+  path: '/', element: <HomePage />, errorElement: <NotFoundPage /> },{
   path: '/weather-app', element: <WeatherApp /> },{
   path: '/social-media', element: <SocialMediaApp /> },{
   path: '/video-shorts' , element: <VideoShorts />, HydrateFallbackElement: VideoShortsLoader },{
   path: '/video-shorts/:id', element: <VideoShortsById /> },{
   path: '/ticket-system', element: <LayOut />,
-  children: [{
-  path: 'admin/login', element: <AdminDashboardPage />, loader: fetchTickets },{
-  path: 'log/ticket', element: <TicketForm /> },{
-  path: 'profile', element: <UserProfilePage />
-  }
-  ]
+    children: [{
+    path: 'admin/login', element: <AdminDashboardPage />, loader: fetchTickets },{
+    path: 'log/ticket', element: <TicketForm /> },{
+    path: 'profile', element: <UserProfilePage />
+    }]
+  }]
 }];
 
 const router = createBrowserRouter(routes);

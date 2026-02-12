76b8467636e2f963d0d74da967a07442606b15e1
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-02-13 00:27:19 +0200

    Added the stats containers

--- root/frontend/src/pages/AdminDashboard.jsx
+++ root/frontend/src/pages/AdminDashboard.jsx
@@ -17,15 +17,63 @@ export default function AdminDashboardPage() {
   const result = useLoaderData();
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Admin Dashboard Page</h1>
-      <div>
-        {result.map((ticket,_id) =>(
-          <div key={_id}>
-            <h2>{ticket.employeeName}</h2>
-            <p>{ticket.employeeEmail}</p>
-          </div>
-        ))}
+    <div className="h-screen flex flex-col gap-20 items-center">
+      <div className="stats shadow">
+        <div className="stat place-items-center">
+          <div className="stat-title">Total Tickets</div>
+          <div className="stat-value">{result.length}</div>
+        </div>
+        <div className="stat place-items-center">
+          <div className="stat-title">Open Tickets</div>
+          <div className="stat-value">{result.filter(t => t.status === "Open").length}</div>
+        </div>
+        <div className="stat place-items-center">
+          <div className="stat-title">Hardware Related</div>
+          <div className="stat-value">{result.filter(t => t.issueType === "Hardware").length}</div>
+        </div>
+        <div className="stat place-items-center">
+          <div className="stat-title">Software Related</div>
+          <div className="stat-value">{result.filter(t => t.issueType === "Software").length}</div>
+        </div>
+        <div className="stat place-items-center">
+          <div className="stat-title">Network Related</div>
+          <div className="stat-value">{result.filter(t => t.issueType === "Network").length}</div>
+        </div>
+        <div className="stat place-items-center">
+          <div className="stat-title">Other Related</div>
+          <div className="stat-value">{result.filter(t => t.issueType === "Other").length}</div>
+        </div>
       </div>
+      <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Issue</th>
+              <th>Description</th>
+              <th>Status</th>
+              <th>Last Updated</th>
+              <th>Reply</th>
+            </tr>
+          </thead>
+          <tbody>
+            {result.map((ticket, _id) => (
+              <tr key={_id}>
+                <td>{ticket.employeeName}</td>
+                <td>{ticket.employeeEmail}</td>
+                <td>{ticket.issueType}</td>
+                <td>{ticket.description}</td>
+                <td>{ticket.status}</td>
+                <td>{ticket.updatedAt}</td>
+                <td> <button 
+                  className="bg-green-500 hover:bg-green-600 disabled:bg-gray-400 disabled:cursor-not-allowed px-4 py-2 cursor-pointer rounded font-bold" disabled={ticket.status === "Closed"}>{ticket.status==="Open" ? "Solve" : "Solved"} </button> </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div> 
     </>
   );
 }

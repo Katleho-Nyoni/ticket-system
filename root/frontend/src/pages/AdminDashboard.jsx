dbdaadbc3cc36d1c9ab060ac5832b3f39108fa54
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-02-12 16:39:29 +0200

    Creating a function that fetches data from backend & display the data in the frontend

--- root/frontend/src/pages/AdminDashboard.jsx
+++ root/frontend/src/pages/AdminDashboard.jsx
@@ -1,7 +1,31 @@
+import { useLoaderData } from "react-router-dom";
+
+export const fetchTickets = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:27027/api/ts/tickets');
+      const results = await response.json();
+      return results;
+    } catch (err) {
+      console.error(err);
+      return [];
+    }
+    
+}
+
+
 export default function AdminDashboardPage() {
+  const result = useLoaderData();
   return (
     <>
       <h1 className="text-3xl font-bold underline">Admin Dashboard Page</h1>
+      <div>
+        {result.map((ticket,_id) =>(
+          <div key={_id}>
+            <h2>{ticket.employeeName}</h2>
+            <p>{ticket.employeeEmail}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }

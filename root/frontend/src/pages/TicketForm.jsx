54176d36fa83eea9429bb38a47fe03b8710d0c73
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-02-20 23:23:19 +0200

    Modifying form fields

--- root/frontend/src/pages/TicketForm.jsx
+++ root/frontend/src/pages/TicketForm.jsx
@@ -19,29 +19,20 @@ export default function TicketForm(){
             <div className="flex flex-col justify-center items-center m-auto mt-30 gap-4 h-70 w-170">
                 <h1 className="text-3xl font-bold">TICKET LOGGER</h1>
                 <Form className="flex flex-col justify-center gap-4 rounded ">
-                    <input type="text" name="name" value={user.displayName} hidden />
-                    <input type="email" name="email" value={user.email} hidden />
+                    <input type="text" name="employeeName" value={user.displayName} hidden />
+                    <input type="email" name="employeeEmail" value={user.email} hidden />
             
                     <summary className="list-none ">
-                    <select name="software-hardware" /*value={issueType}*/  id="software-hardware">
+                    <select name="issueType" /*value={issueType}*/  id="software-hardware">
                         <option value="">default</option>
                         <option value="hardware">Hardware</option>
                         <option value="software">Software</option>
                     </select>
                     </summary>
-            
-                    <summary className="list-none">
-                    <select name="department" id="department">
-                        <option value="">Department</option>
-                        <option value="marketing">Marketing</option>
-                        <option value="human-resource">Human Resource</option>
-                        <option value="tech">Tech</option>
-                    </select>
-                    </summary>
-                
-                    <textarea id="description" name="description" rows={6} placeholder="Leave brief description here..." required className="border border-blue-600 py-2 px-18 rounded"></textarea>
-                
-                    <button type="submit" /*onSubmit={SubmitForm}*/ className="bg-blue-500 text-white font-bold py-2 px-4 w-auto m-6 self-center rounded cursor-pointer hover:bg-blue-600">Submit Ticket</button>
+                    <textarea name="description" rows={6} placeholder="Leave brief description here..." required className="border border-blue-600 py-2 px-18 rounded"></textarea>
+                    <input type="text" name="status" value="Open" hidden />
+                    <button type="submit" 
+                     className="bg-blue-500 text-white font-bold py-2 px-4 w-auto m-6 self-center rounded cursor-pointer hover:bg-blue-600">Submit Ticket</button>
                 </Form>
             </div>
         </>

35ec6499a43cfddef1a0792a7d479aeef26dd213
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-02-27 00:07:46 +0200

    Adding the placeholder text for TS home page

--- root/frontend/src/pages/TicketSystem.jsx
+++ root/frontend/src/pages/TicketSystem.jsx
@@ -9,6 +9,10 @@ export default function LayOut(){
                 <Navigation />
                 <main className="flex-1 p-6">
                     <Outlet />
+                    <div className="h-screen flex flex-col justify-center gap-20">
+                        <h1 className="font-bold text-6xl">Welcome to the Ticket System</h1>
+                        <p className="text-xl">This section will later allow users to use an AI Agent I will build.</p>
+                    </div>
                 </main>
             </div>  
         </>

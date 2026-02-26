fc050b5840e3e2fda89ae8fc9390f890c5369b72
Katleho-Nyoni <eaglenyoni@gmail.com>
2026-02-27 00:08:10 +0200

    Handling forms

--- root/frontend/src/pages/TicketForm.jsx
+++ root/frontend/src/pages/TicketForm.jsx
@@ -1,6 +1,6 @@
 // import { useState } from "react";
 import axios from "axios";
-import { Form } from "react-router-dom";
+import { Form, redirect } from "react-router-dom";
 import { useUser } from "../auth/authenticate";
 
 export default function TicketForm(){
@@ -13,22 +13,30 @@ export default function TicketForm(){
     // function SubmitForm(){
 
     // }
+    if(user===null){
+        return <p>Loading...</p>
+    }
+
+    if(isLoading){
+        return <p>Loading...</p>
+    }
 
     return(
         <>
             <div className="flex flex-col justify-center items-center m-auto mt-30 gap-4 h-70 w-170">
                 <h1 className="text-3xl font-bold">TICKET LOGGER</h1>
-                <Form className="flex flex-col justify-center gap-4 rounded ">
-                    <input type="text" name="employeeName" value={user.displayName} hidden />
-                    <input type="email" name="employeeEmail" value={user.email} hidden />
+                <Form method="post" action="/ticket-system/log/ticket"
+                className="flex flex-col justify-center gap-4 rounded ">
+                    <input type="text" name="employeeName" defaultValue={user.displayName} hidden />
+                    <input type="email" name="employeeEmail" defaultValue={user.email} hidden />
             
-                    <summary className="list-none ">
                     <select name="issueType" /*value={issueType}*/>
-                        <option value="">default</option>
+                        <option value="">Issue Type</option>
                         <option value="hardware">Hardware</option>
                         <option value="software">Software</option>
+                        <option value="network">Network</option>
+                        <option value="other">Other</option>
                     </select>
-                    </summary>
                     <textarea name="description" rows={6} placeholder="Leave brief description here..." required className="border border-blue-600 py-2 px-18 rounded"></textarea>
                     <input type="text" name="status" value="Open" hidden />
                     <button type="submit" 
@@ -37,4 +45,13 @@ export default function TicketForm(){
             </div>
         </>
     );
+}
+
+export const ticketFormAction = async ({request}) => {
+    const formEntry = await request.formData();
+    const ticketEntry = Object.fromEntries(formEntry);
+
+    console.log(ticketEntry);
+
+    return redirect("/ticket-system/admin/login");
 }
